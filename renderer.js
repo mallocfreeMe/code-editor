bac0ea9e92e0776538b44f92d0ee3a8f7aa376b0
jianghui dai <jianghuidai@outlook.com>
2020-04-22 18:41:29 -0700

    add read file functionality

---
+++ renderer.js
@@ -0,0 +1,27 @@
+const fs = require("fs");
+const path = require("path");
+
+document.addEventListener('drop', (e) => {
+  e.preventDefault();
+  e.stopPropagation();
+
+  for (const f of e.dataTransfer.files) {
+    console.log('File(s) you dragged here: ', f.path);
+
+    document.getElementById("file-name").innerHTML = path.parse(f.path).base;
+
+    // Asynchronous read
+    fs.readFile(f.path, function (err, data) {
+      if (err) {
+        return console.error(err);
+      }
+      console.log("Asynchronous read: " + data.toString());
+
+      document.getElementById("file-content").innerHTML = data.toString();
+    });
+  }
+});
+document.addEventListener('dragover', (e) => {
+  e.preventDefault();
+  e.stopPropagation();
+});
