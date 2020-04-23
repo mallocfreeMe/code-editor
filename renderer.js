2c1909c201f9102d09f00b26817cf6a533e25bf5
jianghui dai <jianghuidai@outlook.com>
2020-04-23 00:25:14 -0700

    Add tabs

--- renderer.js
+++ renderer.js
@@ -1,6 +1,10 @@
 const fs = require("fs");
 const path = require("path");
 
+const TabGroup = require("electron-tabs");
+
+let tabGroup = new TabGroup();
+
 let editor = ace.edit("file-content");
 editor.setTheme("ace/theme/twilight");
 editor.session.setMode("ace/mode/javascript");
@@ -12,7 +16,18 @@ document.addEventListener('drop', (e) => {
   for (const f of e.dataTransfer.files) {
     console.log('File(s) you dragged here: ', f.path);
 
-    document.getElementById("file-name").innerHTML = path.parse(f.path).base;
+    const fileName = path.parse(f.path).base;
+
+    let tab = tabGroup.addTab({
+      title: fileName,
+      src: "index.html",
+      visible: true
+    });
+
+    document.getElementById("file-name").innerHTML = fileName;
+
+    appendToSideNav(fileName);
+
 
     // Asynchronous read
     fs.readFile(f.path, function (err, data) {
@@ -22,10 +37,33 @@ document.addEventListener('drop', (e) => {
       console.log("Asynchronous read: " + data.toString());
 
       editor.insert(data.toString());
+
     });
   }
 });
+
 document.addEventListener('dragover', (e) => {
   e.preventDefault();
   e.stopPropagation();
 });
+
+
+function appendToSideNav(fileName) {
+  let ul = document.getElementById("side-nav");
+  let li = document.createElement("li");
+  li.appendChild(document.createTextNode(fileName));
+  li.style.setProperty("list-style", "none");
+  li.style.setProperty("padding-left", "30%");
+  ul.appendChild(li);
+}
+
+function createBrowserWindow() {
+  const remote = require('electron').remote;
+  const BrowserWindow = remote.BrowserWindow;
+  const win = new BrowserWindow({
+    height: 1000,
+    width: 800
+  });
+
+  win.loadURL('<url>');
+}
