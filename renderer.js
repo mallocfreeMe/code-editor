8c02b0344703c2ce6006e336600e8d45710b9330
jianghui dai <jianghuidai@outlook.com>
2020-04-28 00:40:49 -0700

    let side nav avoid adding same path files

--- renderer.js
+++ renderer.js
@@ -4,8 +4,8 @@ const path = require("path");
 let filePath = [];
 
 let editor = ace.edit("file-content");
-editor.setTheme("ace/theme/twilight");
-editor.session.setMode("ace/mode/javascript");
+editor.setTheme("ace/theme/dracula");
+editor.session.setMode("ace/mode/text");
 editor.$blockScrolling = Infinity;
 
 document.addEventListener('drop', (e) => {
@@ -14,19 +14,34 @@ document.addEventListener('drop', (e) => {
 
   for (const f of e.dataTransfer.files) {
 
-    filePath.push(f.path);
-
     const fileName = path.parse(f.path).base;
 
     document.getElementById("file-name").innerHTML = fileName;
 
-    appendToSideNav(fileName);
+    if (filePath.length == 0) {
+      filePath.push(f.path);
+      appendToSideNav(fileName);
+    } else {
+      let isSameFile = false;
+      for (let i = 0; i < filePath.length; i++) {
+        if (filePath[i] == f.path) {
+          isSameFile = true;
+        }
+      }
+      if (!isSameFile) {
+        filePath.push(f.path);
+        appendToSideNav(fileName);
+      }
+    }
 
     // Asynchronous read
     fs.readFile(f.path, function (err, data) {
       if (err) {
         return console.error(err);
       }
+
+      // set current synatax highlighting 
+      setCurrentMode(fileName);
 
       // clean the editor and add drag content to it
       editor.setValue("");
@@ -50,16 +65,29 @@ function appendToSideNav(fileName) {
   li.style.setProperty("padding-left", "30%");
   li.style.setProperty("cursor", "pointer");
   li.setAttribute("id", fileName);
+  li.setAttribute("onmouseover", "this.className='hover'");
+  li.setAttribute("onmouseout", "this.className=''");
   li.addEventListener('click', event => {
     for (let i = 0; i < filePath.length; i++) {
-      if (fileName == path.parse(filePath[i]).base) {
+
+      const currentFileName = path.parse(filePath[i]).base;
+
+      document.getElementById(currentFileName).style.setProperty("background-color", "#21252B");
+
+      if (fileName == currentFileName) {
         // Asynchronous read
         fs.readFile(filePath[i], function (err, data) {
           if (err) {
             return console.error(err);
           }
 
-          // clean the editor and add drag content to it
+          document.getElementById(fileName).style.setProperty("background-color", "#2C313A");
+
+
+          // set current synatax highlighting 
+          setCurrentMode(fileName);
+
+          document.getElementById("file-name").innerHTML = fileName;
           editor.setValue("");
           editor.insert(data.toString());
 
@@ -68,4 +96,22 @@ function appendToSideNav(fileName) {
     }
   });
   ul.appendChild(li);
+}
+
+function setCurrentMode(fileName) {
+  let currentMode = path.extname(fileName);
+
+  switch (currentMode) {
+    case ".html":
+      editor.session.setMode("ace/mode/html");
+      break;
+    case ".css":
+      editor.session.setMode("ace/mode/css");
+      break;
+    case ".js":
+      editor.session.setMode("ace/mode/javascript");
+      break;
+    default:
+      editor.session.setMode("ace/mode/text");
+  }
 }
