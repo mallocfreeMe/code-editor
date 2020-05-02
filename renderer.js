6726ffa5a1b0ba149d85fa793d9a59456cbfbefd
jianghui dai <jianghuidai@outlook.com>
2020-05-01 18:34:08 -0700

    fixed the functionality of reading a folder

--- renderer.js
+++ renderer.js
@@ -35,15 +35,14 @@ document.addEventListener('drop', (e) => {
 
     // detect whether the drop item is a file or a folder
     if (!fs.lstatSync(f.path).isDirectory()) {
-      const fileName = path.parse(f.path).base;
 
-      document.getElementById("file-name").innerHTML = fileName;
+      document.getElementById("file-name").innerHTML = path.parse(f.path).base;
 
       if (filePath.length == 0) {
         filePath.push(f.path);
-        appendToSideNav(fileName);
+        appendToSideNav(f.path);
 
-        document.getElementById(fileName).style.setProperty("background-color", "#2C313A");
+        document.getElementById(f.path).style.setProperty("background-color", "#2C313A");
       } else {
         let isSameFile = false;
         for (let i = 0; i < filePath.length; i++) {
@@ -53,7 +52,7 @@ document.addEventListener('drop', (e) => {
         }
         if (!isSameFile) {
           filePath.push(f.path);
-          appendToSideNav(fileName);
+          appendToSideNav(f.path);
         }
 
         for (let i = 0; i < filePath.length; i++) {
@@ -61,14 +60,14 @@ document.addEventListener('drop', (e) => {
           currentFile.style.setProperty("background-color", "#21252B");
         }
 
-        document.getElementById(fileName).style.setProperty("background-color", "#2C313A");
+        document.getElementById(f.path).style.setProperty("background-color", "#2C313A");
       }
 
       // synchronous read
       const data = fs.readFileSync(f.path);
 
       // set current synatax highlighting 
-      setCurrentMode(fileName);
+      setCurrentMode(f.path);
 
       // clean the editor and add drag content to it
       editor.setValue("");
@@ -86,18 +85,12 @@ document.addEventListener('drop', (e) => {
         if (err) {
           console.log('Error', err);
         } else {
-          filePath.push(f.path);
-          appendToSideNav(path.parse(f.path).base);
-
-          res.forEach(allFilePath => {
-            filePath.push(allFilePath);
-            // appendToSideNav(path.parse(allFilePath).base);
+          fs.readdirSync(f.path).sort(function (a, b) {
+            return a.toLowerCase().localeCompare(b.toLowerCase());
+          }).filter(a => !a.includes(".DS_Store") && !a.includes(".git")).forEach(fileName => {
+            filePath.push(f.path + "/" + fileName);
+            appendToSideNav(f.path + "/" + fileName);
           });
-
-          const homePath = filePath[0];
-          const arr = filePath.filter(fs => (fs.split("/").length - 1 == homePath.split("/").length));
-
-          arr.map(a => appendToSideNav(path.parse(a).base));
         }
       });
 
@@ -134,48 +127,79 @@ editor.on("click", function () {
 })
 
 // append files to the side nav
-function appendToSideNav(fileName) {
+// check if user click a folder or a file
+// if its a file, then read the file and set current synatax highlighting 
+// if its a folder, then open the folder, show all sub files
+function appendToSideNav(fileAdress) {
   let ul = document.getElementById("side-nav");
   let li = document.createElement("li");
-  li.appendChild(document.createTextNode(fileName));
+  li.appendChild(document.createTextNode(path.parse(fileAdress).base));
   li.style.setProperty("list-style", "none");
   li.style.setProperty("padding-left", "30%");
   li.style.setProperty("cursor", "pointer");
-  li.setAttribute("id", fileName);
+  li.setAttribute("id", fileAdress);
   li.setAttribute("onmouseover", "this.className='hover'");
   li.setAttribute("onmouseout", "this.className=''");
 
   li.addEventListener('click', event => {
     for (let i = 0; i < filePath.length; i++) {
 
-      // check if user click a folder or a file
-      // if its a file, then read the file
-      // if its a folder, then open the folder, show all sub files
-      if (!fs.lstatSync(filePath[i]).isDirectory()) {
-        const currentFileName = path.parse(filePath[i]).base;
+      // document.getElementById(path.parse(filePath[i]).base).style.setProperty("background-color", "#21252B");
+      if (fileAdress == filePath[i]) {
 
-        // document.getElementById(currentFileName).style.setProperty("background-color", "#21252B");
+        if (!fs.lstatSync(filePath[i]).isDirectory()) {
 
-        if (fileName == currentFileName) {
-          // synchronous read
           const data = fs.readFileSync(filePath[i]);
 
-          // document.getElementById(fileName).style.setProperty("background-color", "#2C313A");
+          // document.getElementById(fileAdress).style.setProperty("background-color", "#2C313A");
 
+          setCurrentMode(fileAdress);
 
-          // set current synatax highlighting 
-          setCurrentMode(fileName);
-
-          document.getElementById("file-name").innerHTML = fileName;
+          document.getElementById("file-name").innerHTML = path.parse(fileAdress).base;
           editor.setValue("");
           editor.insert(data.toString());
           editor.gotoLine(1, 0);
           const row = 1;
           const col = 0;
           document.getElementById("cursor-pos").innerHTML = "Ln " + row + ", " + "Col " + col;
+        } else {
+          // const arr = filePath.filter(fs => (fs.split("/").length - 1 == homePath.split("/").length));
+          let flag = true;
+
+          filePath.filter(a => a.includes(filePath[i])).forEach(a => {
+            if (a.split("/").length - 1 > filePath[i].split("/").length - 1) {
+              flag = false;
+            }
+          });
+
+          if (flag) {
+            read(filePath[i]);
+          } else {
+
+            let flag2 = false;
+
+            filePath.filter(a => a.includes(filePath[i])).filter(a => a.split("/")
+              .length - 1 > filePath[i].split("/").length - 1)
+              .forEach(a => {
+                if (document.getElementById(a).style.getPropertyValue("display") == "block") {
+                  flag2 = true;
+                } else {
+                  flag2 = false;
+                }
+              });
+
+            if (flag2) {
+              filePath.filter(a => a.includes(filePath[i])).filter(a => a.split("/")
+                .length - 1 > filePath[i].split("/").length - 1)
+                .forEach(a => document.getElementById(a).style.setProperty("display", "none"));
+            } else {
+              filePath.filter(a => a.includes(filePath[i])).filter(a => a.split("/")
+                .length - 1 > filePath[i].split("/").length - 1)
+                .forEach(a => document.getElementById(a).style.setProperty("display", "block"));
+            }
+          }
+
         }
-      } else {
-        console.log("it's folder");
       }
 
     }
@@ -184,8 +208,8 @@ function appendToSideNav(fileName) {
 }
 
 // detect current file syntax
-function setCurrentMode(fileName) {
-  let currentMode = path.extname(fileName);
+function setCurrentMode(filePath) {
+  let currentMode = path.extname(path.parse(filePath).base);
 
   switch (currentMode) {
     case ".html":
@@ -204,4 +228,13 @@ function setCurrentMode(fileName) {
       editor.session.setMode("ace/mode/text");
       document.getElementById("file-format").innerHTML = "Plain Text";
   }
+}
+
+function read(path) {
+  fs.readdirSync(path).sort(function (a, b) {
+    return a.toLowerCase().localeCompare(b.toLowerCase());
+  }).filter(a => !a.includes(".DS_Store") && !a.includes(".git")).forEach(fileName => {
+    filePath.push(path + "/" + fileName);
+    appendToSideNav(path + "/" + fileName);
+  });
 }
